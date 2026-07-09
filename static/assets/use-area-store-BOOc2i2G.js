330071f25dc0ef4f69cb4e1dfffd6ba2728572f2
Vicente Ignacio Lincoqueo Roa <vitcho123@gmail.com>
2026-07-09 16:34:24 -0300

    release: implement dynamic AI model synchronization

---
+++ static/assets/use-area-store-BOOc2i2G.js
@@ -0,0 +1,1 @@
+import{_ as r,$ as s}from"./index-FYJvAgVa.js";const l=r()(s(e=>({selectedAreaId:null,setSelectedArea:a=>e({selectedAreaId:a})}),{name:"lab-area"}));export{l as u};
--- static/assets/use-area-store-OFsNzrpo.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,$ as s}from"./index-BQPO8Bd_.js";const l=r()(s(e=>({selectedAreaId:null,setSelectedArea:a=>e({selectedAreaId:a})}),{name:"lab-area"}));export{l as u};

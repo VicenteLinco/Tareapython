e84a23e61b25553c625cf1065d42d65c0aa29412
Vicente Ignacio Lincoqueo Roa <vitcho123@gmail.com>
2026-07-09 17:19:05 -0300

    release: implement github models support and dynamic provider/model overrides in uploader modal

--- static/assets/skeleton-CE4h1shE.js
+++
@@ -1,1 +0,0 @@
-import{j as t,d as n}from"./index-CS3YT2zF.js";function r({className:s,...e}){return t.jsx("div",{className:n("skeleton",s),...e})}export{r as S};
---
+++ static/assets/use-area-store-LHrzehGf.js
@@ -0,0 +1,1 @@
+import{_ as r,$ as s}from"./index-BGWrfmtD.js";const l=r()(s(e=>({selectedAreaId:null,setSelectedArea:a=>e({selectedAreaId:a})}),{name:"lab-area"}));export{l as u};

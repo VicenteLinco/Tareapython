9e9994c89cd494df7e5f6f56da3cbd70ed5bb4ba
Vicente Ignacio Lincoqueo Roa <vitcho123@gmail.com>
2026-07-09 17:39:18 -0300

    release: add PDF support alert note in uploader modal for OpenAI compatibility limits

---
+++ static/assets/skeleton-eBZ-EbBQ.js
@@ -0,0 +1,1 @@
+import{j as t,d as n}from"./index-fp46LRfB.js";function r({className:s,...e}){return t.jsx("div",{className:n("skeleton",s),...e})}export{r as S};
---
+++ static/assets/use-area-store-DmSaDXnJ.js
@@ -0,0 +1,1 @@
+import{_ as r,$ as s}from"./index-fp46LRfB.js";const l=r()(s(e=>({selectedAreaId:null,setSelectedArea:a=>e({selectedAreaId:a})}),{name:"lab-area"}));export{l as u};

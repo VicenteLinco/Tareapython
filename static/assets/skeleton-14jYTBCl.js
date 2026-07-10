ea8e69879972b33f2ae15f75a36f3e20671433ea
Vicente Ignacio Lincoqueo Roa <vitcho123@gmail.com>
2026-07-09 21:57:36 -0300

    release: remove WhatsApp configuration and implement custom models list manager

---
+++ static/assets/skeleton-14jYTBCl.js
@@ -0,0 +1,1 @@
+import{j as t,d as n}from"./index-CMCm68Lq.js";function r({className:s,...e}){return t.jsx("div",{className:n("skeleton",s),...e})}export{r as S};
--- static/assets/skeleton-BB4r_6jE.js
+++
@@ -1,1 +0,0 @@
-import{j as t,d as n}from"./index-ByNpBf8A.js";function r({className:s,...e}){return t.jsx("div",{className:n("skeleton",s),...e})}export{r as S};

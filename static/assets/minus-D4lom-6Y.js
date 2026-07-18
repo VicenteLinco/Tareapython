131c05ad5a391844ff4f0b33ffb3caf693ff653b
Vicente Ignacio Lincoqueo Roa <vitcho123@gmail.com>
2026-07-17 21:12:05 -0300

    build: compile both Create and Edit forms with tabbed layout

--- static/assets/minus-D4lom-6Y.js
+++
@@ -1,6 +0,0 @@
-import{d as o}from"./index-GUP2Y0Cq.js";/**
- * @license lucide-react v0.577.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const s=[["path",{d:"M5 12h14",key:"1ays0h"}]],e=o("minus",s);export{e as M};
--- static/assets/skeleton-Da2plTRh.js
+++
@@ -1,1 +0,0 @@
-import{j as t,e as n}from"./index-GUP2Y0Cq.js";function r({className:e,...s}){return t.jsx("div",{className:n("skeleton",e),...s})}export{r as S};
